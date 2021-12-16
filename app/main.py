20465344100418d7f639cec8a4253a0674d15593
mushroomsundays <mushroomsundays@protonmail.com>
2021-12-16 11:16:45 -0500

    cleaned up main function; added get_prices and update_sheet

--- app/main.py
+++ app/main.py
@@ -35,47 +35,76 @@ DOC_NAME = 'defi_master'
 SHEET = 'sheet1'
 MIN_HEALTH_FACTOR = 1.8
 
-def get_prices():
-    pass 
+def get_prices(num_tries=3, delay=30):
+    """
+    Returns a dictionary like {'ETH': 'usd', 4039.02}
+    """
+    # get current crypto prices
+    while num_tries > 0:
+        try:
+            prices_dict = get_current_prices(
+                currencies=CURRENCIES,
+                vs_currencies='usd'
+            )
+            # continue when we successfully get prices
+            num_tries = 0
+            logging.info(f"Prices fetched for {CURRENCIES}")
 
-def update_sheet():
-    pass
+            # make dict with tickers as keys instead of CoinGecko coins
+            ticker_prices_dict = defaultdict()
+            for i,k in enumerate(prices_dict.keys()):
+                ticker = currency_map[k]
+                ticker_prices_dict[ticker] = prices_dict[k]
+            
+            return ticker_prices_dict
+
+        except ConnectionResetError:
+            logging.info(f"CoinGecko ping failed. Waiting 30s...")
+            num_tries -= 1
+            time.sleep(delay)
+    return
+
+def update_sheet(df: pd.DataFrame, ticker_prices_dict: dict) -> None:
+    # loop through sheet and update prices
+    try:
+        tickers = [ currency_map[x] for x in CURRENCIES ]
+    except KeyError:
+        logging.info("One or more of your currencies are not keys in currency_map")
+
+    for i,row in df.iterrows():
+        for j,cell in enumerate(row):
+            if cell in tickers: # check for cells matching valid tickers
+                # store the cell coordinate e.g. D2
+                ticker_coordinate = alphabet_map[j] + str(i+1)
+                logging.info(f"Ticker {cell} found at {ticker_coordinate}")
+
+                # update the adjascent cell with the new price
+                price_coordinate = alphabet_map[j+1] + str(i+1)
+                new_price = ticker_prices_dict[cell]['usd']
+
+                fill_cell(
+                    doc_name=DOC_NAME,
+                    sheet=SHEET,
+                    key_cell=price_coordinate,
+                    value=new_price
+                )
+                logging.info(f"{price_coordinate} updated to {new_price}. Sleeping for 3s...")
+                time.sleep(3)
+
+    logging.info("Done updating sheet.")
+    time.sleep(3)
 
 def main():
-    # NOTE: assumes prices are to the right of cells with currency tickers
-    # while True...
-    # search sheet for currency tickers
-    # (think of a better way than looping through all cells)
-    # store in a dict {cell: new_value}
-    # loop through dict and call fill_cell()
+    """
+    Assumes prices are located in cells adjascent (to the right) of tickers
+    """
 
     global MIN_HEALTH_FACTOR
     
     while True:
-        # get current crypto prices
-        # try 3 times with 30s sleep in between
-        num_tries = 3
-        while num_tries > 0:
-            try:
-                prices_dict = get_current_prices(
-                    currencies=CURRENCIES,
-                    vs_currencies='usd'
-                )
-                # continue when we successfully get prices
-                num_tries = 0
-                logging.info(f"Prices fetched for {CURRENCIES}")
-            except ConnectionResetError:
-                logging.info(f"CoinGecko ping failed. Waiting 30s...")
-                num_tries -= 1
-                time.sleep(30)
+        ticker_prices_dict = get_prices()
 
-        # make dict with tickers as keys instead of CoinGecko coins
-        ticker_prices_dict = defaultdict()
-        for i,k in enumerate(prices_dict.keys()):
-            ticker = currency_map[k]
-            ticker_prices_dict[ticker] = prices_dict[k]
-
-        # read spreadsheet containing tickers & prices
+        # read current sheet and update prices
         df = read_sheet(
             doc_name=DOC_NAME,
             sheet=SHEET,
@@ -83,9 +112,13 @@ def main():
         )
         logging.info(f"{SHEET} from {DOC_NAME} read")
 
-        # check Health column for factors < MIN_HEALTH_FACTOR
-        # TODO: health factor stayed at 1.9 after error and restart
-        # TODO: can I do health factor check AFTER looping through sheet?
+        # update prices
+        update_sheet(df, ticker_prices_dict)
+
+        # read sheet again before checking health factors
+
+        # MIN_HEALTH_FACTOR decreases by 10% every time an email is sent
+        logging.info("Checking health factors...")
         is_healthy = check_health_factors(
             df=df,
             min_health_factor=MIN_HEALTH_FACTOR,
@@ -102,34 +135,7 @@ def main():
             # decrease min health factor by 10% after an email
             MIN_HEALTH_FACTOR = MIN_HEALTH_FACTOR * 0.9
             logging.info(f"Minimum health factor decreased to {MIN_HEALTH_FACTOR}")
-
-        # loop through sheet and update prices
-        try:
-            tickers = [ currency_map[x] for x in CURRENCIES ]
-        except KeyError:
-            logging.info("One or more of your currencies are not keys in currency_map")
-
-        for i,row in df.iterrows():
-            for j,cell in enumerate(row):
-                if cell in tickers: # check for cells matching valid tickers
-                    # store the cell coordinate e.g. D2
-                    ticker_coordinate = alphabet_map[j] + str(i+1)
-                    logging.info(f"Ticker {cell} found at {ticker_coordinate}")
-
-                    # update the adjascent cell with the new price
-                    price_coordinate = alphabet_map[j+1] + str(i+1)
-                    new_price = ticker_prices_dict[cell]['usd']
-
-                    fill_cell(
-                        doc_name=DOC_NAME,
-                        sheet=SHEET,
-                        key_cell=price_coordinate,
-                        value=new_price
-                    )
-                    logging.info(f"{price_coordinate} updated to {new_price}. Sleeping for 3s...")
-                    time.sleep(3)
-
-        logging.info("Sleeping for 60 seconds...")
+        
         time.sleep(60)
 
     # TODO: send email on termination; sleep for X and then retry
