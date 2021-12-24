072a1c4c8cb7f088d23fc6cd1c99da9a409986f3
mushroomsundays <mushroomsundays@protonmail.com>
2021-12-24 09:41:43 -0500

    removed real email

--- poc/email_notification.py
+++ poc/email_notification.py
@@ -3,8 +3,8 @@ from simplegmail import Gmail
 gmail = Gmail() # will open a browser window to ask you to log in and authenticate
 
 params = {
-  "to": "jmoore87jr@gmail.com",
-  "sender": "jmoore87jr@gmail.com",
+  "to": "emailTo@gmail.com",
+  "sender": "emailFrom@gmail.com",
   "subject": "My first email with simplegmail library",
   "msg_html": "<h1>Woah, my first email!</h1><br />This is an HTML email.",
   "msg_plain": "Hi\nThis is a plain text email.",
